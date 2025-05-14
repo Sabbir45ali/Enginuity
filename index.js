a614b9cba0e04d57af62421a2f07f8fd606c798d
Sabbir45ali <sabbir84b@gmail.com>
2025-05-14 16:13:00 +0530

    Add mobile menu toggle functionality

--- index.js
+++ index.js
@@ -63,3 +63,9 @@ function toggleScrollBtn() {
   updateActiveNav();
   toggleScrollBtn();
 });
+
+// Toggle for mobile menu
+document.getElementById('menu-toggle').addEventListener('click', function () {
+  const mobileMenu = document.getElementById('mobile-menu');
+  mobileMenu.classList.toggle('hidden');
+});
